8f27d53004c7285023234349ee1ac7f64af5a169
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2025-08-18 14:49:40 +0200

    Exclude config files from coverage

--- packages/v-tabler/vitest.config.js
+++ packages/v-tabler/vitest.config.js
@@ -10,7 +10,19 @@ export default defineConfig(configEnv =>
             test: {
                 environment: 'jsdom',
                 exclude: [...configDefaults.exclude, 'e2e/*'],
-                root: fileURLToPath(new URL('./', import.meta.url))
+                root: fileURLToPath(new URL('./', import.meta.url)),
+                coverage: {
+                    exclude: [
+                        ...configDefaults.exclude,
+                        ...configDefaults.coverage.exclude,
+                        'histoire.config.js',
+                        'histoire.setup.js',
+                        'uno.config.js',
+                        '**/demo/**',
+                        'src/install.js',
+                        'src/index.js'
+                    ]
+                }
             }
         })
     )
