491631835dbea46f3e12fb3e145d3511a9f73dcd
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2025-09-16 09:46:39 +0200

    Watch current tab index in tabs components

--- packages/v-tabler/src/components/card/TabCardComponent.vue
+++ packages/v-tabler/src/components/card/TabCardComponent.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="card">
         <div class="pt-3 bg-surface">
-            <tabs-component @update:current-tab-index="selectTab" :tabs="tabs">
-            </tabs-component>
+            <tabs-component @update:current-tab-index="selectTab" :tabs="tabs"> </tabs-component>
         </div>
 
         <keep-alive v-if="keepAlive">
             <component :is="component" v-bind="componentProps" v-on="componentEvents"></component>
         </keep-alive>
-        <component v-else :is="component" v-bind="componentProps" v-on="componentEvents"></component>
-
+        <component
+            v-else
+            :is="component"
+            v-bind="componentProps"
+            v-on="componentEvents"
+        ></component>
     </div>
 </template>
 <script setup>
 import TabsComponent from '../tabs/TabsComponent.vue'
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 const props = defineProps({
     currentTabIndex: {
         type: Number,
@@ -33,6 +36,14 @@ const component = computed(() => props.tabs[tabIndex.value].component)
 const componentProps = computed(() => props.tabs[tabIndex.value].props)
 const componentEvents = computed(() => props.tabs[tabIndex.value].events || {})
 const tabIndex = ref(props.currentTabIndex)
+
+watch(
+    () => props.currentTabIndex,
+    newIndex => {
+        tabIndex.value = newIndex
+    }
+)
+
 const selectTab = index => {
     tabIndex.value = index
 }
